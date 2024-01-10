b66ac2e5c44b05593a563fc11cd5e89da3d21b90
tr1b1iwnl <tr1b1iwnl@gmail.com>
2024-01-11 01:38:13 +0800

    Modified axios origin

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -5,8 +5,10 @@ const axiosInstance = axios.create({
     process.env.NODE_ENV === "production"
       ? process.env.REACT_APP_BOARDPREP_API
       : "http://localhost:8000",
+  withCredentials: true,
   headers: {
     "Access-Control-Allow-Credentials": true,
+    "Access-Control-Allow-Origin": "*",
     Origin: "https://boardprep.vercel.app",
   },
 });
