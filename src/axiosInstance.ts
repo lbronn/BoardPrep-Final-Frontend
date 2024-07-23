e2fc80ee6b671af06f22c69c3624867bdc78a376
Le Bronn <lebronn.samson@cit.edu>
2024-07-24 02:32:01 +0800

    Changed to production from deployment phase, modified some lines to incorporate with the change.

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,10 +1,20 @@
 import axios from "axios";
+ 
+// --for deployment i think (dont erase)-- //
+// const axiosInstance = axios.create({ 
+//   baseURL:
+//     process.env.NODE_ENV === "production"
+//       ? process.env.REACT_APP_BOARDPREP_API
+//       : "https://boardprep-backend.azurewebsites.net",
+//   withCredentials: true,
+// });
 
+// remove after testing
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
       ? process.env.REACT_APP_BOARDPREP_API
-      : "https://boardprep-backend.azurewebsites.net",
+      : "http://127.0.0.1:8000",
   withCredentials: true,
 });
 
